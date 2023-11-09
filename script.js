26c6dacbd4700bb68c76e1cead60abe42cf7bcc2
Hrutvi Barad <135630264+hbarad08@users.noreply.github.com>
2023-11-09 07:37:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+const attemptsLeftSpan = document.getElementById("attemptsLeft");
+const wordDisplayDiv = document.getElementById("wordDisplay");
+const guessInput = document.getElementById("guessInput");
+const messageParagraph = document.getElementById("message");
 const pumpkinHeadButton = document.getElementById("pumpkin-head-button");
 const head = document.getElementById("head");
+const makeGuessButton = document.getElementById("makeGuessButton");
 
 pumpkinHeadButton.addEventListener("click", changeHeadToPumpkin);
 
 function changeHeadToPumpkin() {
     head.style.backgroundColor = "#FF6347"; // Change to pumpkin color
     pumpkinHeadButton.disabled = true; // Disable the button after changing the head
+}
+function updateAttemptsLeft(attemptsLeft) {
+    attemptsLeftSpan.textContent = attemptsLeft;
+}
+
+function updateWordDisplay(word) {
+    wordDisplayDiv.textContent = word;
+}
+
+function showMessage(message) {
+    messageParagraph.textContent = message;
 }
