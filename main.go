b9a4b55e45ac9992a4034a9e2df106aade73eeef
wanderhere <5000degreez@gmail.com>
2023-11-26 18:59:13 -0500

    Implemented new design in Go, added go.mod, Go and JS communicate

--- main.go
+++ main.go
@@ -1,132 +1,117 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
+	"encoding/json"
+	"html/template"
 	"math/rand"
-	"os"
+	"net/http"
 	"strings"
 	"time"
 )
 
-// short cut for print next line
-var pl = fmt.Println
+var options = map[string][]string{
 
-// list of words to guess
-var wordList = []string{"programming", "hangman", "golang", "example", "developer"}
+	"fruits":    {"Apple", "Blueberry", "Mandarin", "Pineapple", "Pomegranate", "Watermelon"},
+	"animals":   {"Hedgehog", "Rhinoceros", "Squirrel", "Panther", "Walrus", "Zebra"},
+	"countries": {"India", "Hungary", "Kyrgyzstan", "Switzerland", "Zimbabwe", "Dominica"},
+}
+
+var chosenWord string
+
+var templates = template.Must(template.New("html").Parse(`
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Hangman</title>
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="/static/style.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div id="options-container"></div>
+      <div id="letter-container" class="letter-container hide"></div>
+      <div id="user-input-section"></div>
+      <canvas id="canvas"></canvas>
+      <div id="new-game-container" class="new-game-popup hide">
+        <div id="result-text"></div>
+        <button id="new-game-button">New Game</button>
+      </div>
+    </div>
+    <!-- Script -->
+    <script src="/static/script.js"></script>
+  </body>
+</html>
+
+`))
 
 func main() {
+
 	rand.Seed(time.Now().UnixNano())
-	wordToGuess := getRandomWord()
-	guesses := make([]string, 0)
-	maxAttempts := 6 // maximum tries given by system
-	attempts := 0    // number of tries taken by guess
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/generate-word", generateWordHandler)
 
-	pl("You have %d attempts to guess the word.\n", maxAttempts)
-
-	for {
-		displayWord(wordToGuess, guesses)
-		pl("Enter a letter:")
-		guess := getUserInput()
-		guesses = append(guesses, guess)
-
-		/* if player input the letter which is not included in the given word
-		increment their tries
-		decrease the maximum tries
-		*/
-		if !strings.Contains(wordToGuess, guess) { // Contains is a built in func
-			attempts++
-			pl("Oops!!! Hang in there. Only /%d attempts left\n", maxAttempts-attempts)
-		}
-
-		if isWon(wordToGuess, guesses) {
-			displayWord(wordToGuess, guesses)
-			pl("Congratulations! You've won!")
-			break
-		}
-
-		if attempts == maxAttempts {
-			pl("No more attempts ^w^ Hang the man!!!")
-
-			//Ask if the player wants to play again
-			pl("Do you want to play again? (yes/no)")
-			playAgain := getUserInput()
-			if playAgain != "yes" {
-				pl("Thanks for playing!")
-				break
-			} else {
-				// Reset game variables for a new game
-				wordToGuess = getRandomWord()
-				guesses = make([]string, 0)
-				attempts = 0
-				pl("You have %d attempts to guess the word.\n", maxAttempts)
+	// Enable CORS
+	corsHandler := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
 			}
-		}
+			h.ServeHTTP(w, r)
+		})
 	}
-}
 
-func displayWord(wordToGuess string, guesses []string) {
-	display := " "
-	for _, letter := range wordToGuess {
-		if contains(guesses, string(letter)) {
-			display += string(letter)
-		} else {
-			display += "_"
-		}
-	}
-	pl(display)
-}
+	http.Handle("/", corsHandler(http.HandlerFunc(handler)))
+	http.Handle("/generate-word", corsHandler(http.HandlerFunc(generateWordHandler)))
 
-func isWon(wordToGuess string, guesses []string) bool {
-	for _, letter := range wordToGuess {
-		if !contains(guesses, string(letter)) {
-			return false
-		}
-	}
-	return true
-}
-
-func getUserInput() string {
-	reader := bufio.NewReader(os.Stdin)   // reader works as scanner in Java
-	guess, err := reader.ReadString('\n') // guess is where the user input stored, err is to protected guess from error
-	if err != nil {                       // if err is not null
-		log.Fatal(err)
-	}
-	return strings.TrimSpace(strings.ToLower(guess)) // if there is no error, return the guess with null error
+	http.ListenAndServe(":8080", nil)
 
 }
 
-func getRandomWord() string {
-	return wordList[rand.Intn(len(wordList))]
-}
+func handler(w http.ResponseWriter, r *http.Request) {
 
-// check if the guess is matched with the word to guess
-func contains(wordToGuess []string, item string) bool {
-	for _, letter := range wordToGuess {
-		if letter == item {
-			return true
-		}
-	}
-	return false
+	err := templates.ExecuteTemplate(w, "html", nil)
+	if err != nil {
 
-}
-
-// isLetter checks if the input is a single letter.
-func isLetter(input string) bool {
-	return len(input) == 1 && ('a' <= input[0] && input[0] <= 'z' || 'A' <= input[0] && input[0] <= 'Z')
-}
-
-// displayHangman displays a simple ASCII art representation of the hangman's progress.
-func displayHangman(attempts, maxAttempts int) {
-	switch attempts {
-	case 1:
-		fmt.Println("  ___ ")
-		fmt.Println(" |   |")
-		fmt.Println(" |   O")
-		fmt.Println(" |")
-		fmt.Println(" |")
-		fmt.Println("_|_")
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 
 	}
+
+}
+
+func generateWordHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "OPTIONS" {
+		// Handle preflight request
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// Handle the actual request
+	optionValue := r.FormValue("option")
+	optionArray, ok := options[optionValue]
+
+	if !ok {
+		http.Error(w, "Invalid option", http.StatusBadRequest)
+		return
+	}
+
+	chosenWord = optionArray[rand.Intn(len(optionArray))]
+	chosenWord = strings.ToUpper(chosenWord)
+
+	// Send the chosen word back to the client
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"chosenWord": chosenWord})
 }
